292695e09cb75f44a6269079fc1d40340a723e15
sai tharun kollamaplly <skollamp@progress.com>
2017-06-20 07:29:30 +0000

    Changes made by sai tharun kollamaplly (skollamp@progress.com).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,8 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
         var app = new kendo.mobile.Application(document.body, { skin: "nova" });
+        
+                });
         navigator.splashscreen.hide();
     });
-}());
+} ());
